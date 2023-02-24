79930dfda3aff5cff66d189142c58d8286068c3c
tinydead <91645249+tinydead@users.noreply.github.com>
2023-02-24 14:04:08 +0700

    First version

---
+++ script.js
@@ -0,0 +1,569 @@
+const dataQuestion = [{
+  question: 'Việt Nam gia nhập tổ chức Thương mại Thế giới (WTO) vào năm nào và là thành viên thứ mấy của tổ chức này',
+  a: 'Năm 2006, thành viên thứ 150',
+  b: 'Năm 2006, thành viên thứ 151',
+  c: 'Năm 2007, thành viên thứ 150',
+  d: 'Năm 2007, thành viên thứ 15',
+  answer: 'c',
+},{
+  question: 'TP.HCM thuộc khu vực nào?',
+  a: 'Đông Nam Bộ',
+  b: 'Tây Nam Bộ',
+  c: 'Tây Nguyên',
+  d: 'Duyên hải miền Trung',
+  answer: 'a',
+},{
+  question: 'TP.HCM có bao nhiêu đơn vị hành chính?',
+  a: '24',
+  b: '22',
+  c: '25',
+  d: '23',
+  answer: 'b',
+},
+{
+  question: 'Thủ tướng nước Cộng hòa Xã hội Chủ nghĩa Việt Nam hiện nay là:',
+  a: 'Ông Nguyễn Phú Trọng',
+  b: 'Ông Nguyễn Xuân Phúc',
+  c: 'Ông Phạm Minh Chính',
+  d: 'Ông Nguyễn Sinh Hùng',
+  answer: 'c',
+},{
+  question: 'Thành phố Thủ Đức được thành lập vào năm nào?',
+  a: 'Năm 2018',
+  b: 'Năm 2019',
+  c: 'Năm 2020',
+  d: 'Năm 2021',
+  answer: 'd',
+},{
+  question: 'Hiện nay, Việt Nam có bao nhiêu tỉnh/thành phố',
+  a: '62',
+  b: '63',
+  c: '64',
+  d: '65',
+  answer: 'b',
+},{
+  question: '“Chiếu dời đô” của vua Lý Công Uẩn ra đời vào năm:',
+  a: '1010',
+  b: '1011',
+  c: '1100',
+  d: '1101',
+  answer: 'a',
+},
+{
+  question: 'Tác phẩm “Hịch tướng sĩ” của tác giả:',
+  a: 'Nguyễn Trãi',
+  b: 'Ngô Quyền',
+  c: 'Trần Hưng Đạo',
+  d: 'Đinh Bộ Lĩnh',
+  answer: 'c',
+},{
+  question: 'Tác phẩm nào sau đây của Nguyễn Trãi:',
+  a: 'Hịch tướng sĩ',
+  b: 'Truyện Kiều',
+  c: 'Bình Ngô đại cáo',
+  d: 'Chinh phụ ngâm',
+  answer: 'c',
+},{
+  question: 'Đỉnh núi cao nhất Việt Nam mang tên',
+  a: 'Đỉnh Langbiang',
+  b: 'Đỉnh Hoàng Liên Sơn',
+  c: 'Đỉnh Phanxipăng',
+  d: 'Đỉnh Trường Sơng',
+  answer: 'c',
+},{
+  question: 'Nhà văn được mệnh danh là “Ông vua phóng sự” những năm 1930 nổi tiếng với tác phẩm “Số đỏ” là:',
+  a: 'Vũ Trọng Phụng',
+  b: 'Nam Cao',
+  c: 'Vũ Đình Chí',
+  d: 'Vũ Bằng',
+  answer: 'a',
+},{
+  question: 'Ngày Quốc tế phòng chống AIDS hằng năm là ngày:',
+  a: '30/11',
+  b: '06/5',
+  c: '05/6',
+  d: '01/12',
+  answer: 'd',
+},{
+  question: 'Vịnh Hạ Long – di sản tự nhiên thế giới được UNESCO công nhận hiện thuộc tỉnh nào?',
+  a: 'Tỉnh Quảng Ninh',
+  b: 'Tỉnh Cao Bằng',
+  c: 'Tỉnh Điện Biên',
+  d: 'Tỉnh Sơn La',
+  answer: 'a',
+},{
+  question: 'Bến Nhà Rồng là nơi gắn liền với nhân vật lịch sử',
+  a: 'Mai Chí Thọ',
+  b: 'Võ Nguyên Giá',
+  c: 'Nguyễn Hữu Cảnh',
+  d: 'Nguyễn Tất Thành',
+  answer: 'd',
+},
+{
+  question: 'Tên khai sinh của Bác Hồ là:',
+  a: 'Nguyễn Sinh Sắc',
+  b: 'Hồ Chí Minh',
+  c: 'Nguyễn Ái Quốc',
+  d: 'Nguyễn Sinh Cung',
+  answer: 'd',
+},{
+  question: 'Chủ tịch UBND TP.HCM hiện nay là:',
+  a: 'Ông Phan Văn Mãi',
+  b: 'Ông Nguyễn Thiện Nhân',
+  c: 'Ông Dương Anh Đức',
+  d: 'Ông Nguyễn Văn Nên',
+  answer: 'a',
+},{
+  question: 'Cơ sở chính của Trường Đại học Sài Gòn ở đâu?',
+  a: '273 An Dương Vương, Q.5',
+  b: '105 Bà Huyện Thanh Quan, Q.33',
+  c: '04 Tôn Đức Thắng, Q.1',
+  d: '20 Ngô Thời Nhiệm, Q.3',
+  answer: 'a',
+},{
+  question: 'Các phương thức tuyển sinh của Trường Đại học Sài Gòn?',
+  a: 'Xét tuyển từ kết quả Kì thi đánh giá năng lực của ĐHQG TP.HCM và Kì thi đánh giá đầu vào trên máy tính của Trường Đại học Sài Gòn năm 2023 đối với các ngành không thuộc nhóm ngành đào tạo giáo viên; Xét tuyển sử dụng kết quả Kì thi tốt nghiệp THPT năm 2023.',
+  b: 'Xét tuyển từ kết quả Kì thi đánh giá năng lực của ĐHQG TP.HCM năm 2023 đối với các ngành không thuộc nhóm ngành đào tạo giáo viên; Xét tuyển sử dụng kết quả Kì thi tốt nghiệp THPT năm 2023; Xét tuyển từ học bạ Trung học phổ thông.',
+  c: 'Xét tuyển sử dụng kết quả Kì thi tốt nghiệp THPT năm 2023; Xét tuyển từ học bạ Trung học phổ thông.',
+  d: 'Xét tuyển từ kết quả Kì thi đánh giá năng lực của ĐHQG TP.HCM năm 2023 đối với các ngành không thuộc nhóm ngành đào tạo giáo viên; Xét tuyển sử dụng kết quả Kì thi tốt nghiệp THPT năm 2023',
+  answer: 'a',
+},
+{
+  question: 'Trường Đại học Sài Gòn có xét tuyển từ học bạ Trung học phổ thông đối với các ngành Đại học hệ chính quy năm 2023 không?',
+  a: 'Có',
+  b: 'Không',
+  c: 'Đang xem xét',
+  d: null,
+  answer: 'b',
+},{
+  question: 'Trường Đại học Sài Gòn có quy đổi chứng chỉ TOEFL, IELTS thành điểm xét tuyển môn Tiếng Anh ở các tổ hợp xét tuyển đối với thí sinh được miễn thi môn ngoại ngữ trong kì thi tốt nghiệp THPT năm 2023 theo quy định của Bộ GD&ĐT không?',
+  a: 'Có',
+  b: 'Không',
+  c: 'Đang xem xét.',
+  d: null,
+  answer: 'a',
+},{
+  question: 'Hiện nay, Hiệu trưởng của Trường Đại học Sài Gòn là?',
+  a: 'Ông Phạm Hoàng Quân',
+  b: 'Ông Võ Văn Thật',
+  c: 'Bà Lê Chi Lan',
+  d: 'Ông Lê Minh Triết',
+  answer: 'a',
+},{
+  question: 'Trường Đại học Sài Gòn hiện có bao nhiêu khoa?',
+  a: '18',
+  b: '17',
+  c: '16',
+  d: '15',
+  answer: 'a',
+},{
+  question: 'Trang Facebook chính thức có dấu tích xanh của Trường Đại học Sài Gòn mang tên?',
+  a: 'Trường Đại học Sài Gòn',
+  b: 'Đại học Sài Gòn',
+  c: 'SaiGon Uni – SGU',
+  d: 'Đại học Sài Gòn (SGU)',
+  answer: 'a',
+},{
+  question: 'Trường Đại học Sài Gòn hiện đào tạo các cấp bậc nào sau đây?',
+  a: 'Tiểu học, Trung học cơ sở, Trung học phổ thông, Đại học, Sau Đại học',
+  b: 'Trung cấp, Cao đẳng, Đại học, Sau Đại học',
+  c: 'Cao đẳng, Đại học, Sau Đại học',
+  d: 'Trung cấp, Cao đẳng, Đại học',
+  answer: 'a',
+},{
+  question: 'Trường Đại học Sài Gòn trực thuộc cơ quan nào?',
+  a: 'Bộ Giáo dục và Đào tạo',
+  b: 'Sở Giáo dục và Đào tạo Thành phố Hồ Chí Minh',
+  c: 'Bộ thông tin và truyền thông',
+  d: 'Ủy Ban nhân dân Thành phố Hồ Chí Minh',
+  answer: 'd',
+},{
+  question: 'Các ngành đào tạo giáo viên của Trường Đại học Sài Gòn có xét tuyển từ kết quả Kì thi đánh giá năng lực của ĐHQG TP.HCM năm 2023 không?',
+  a: 'Có',
+  b: 'Không',
+  c: 'Đang xem xét',
+  d: null,
+  answer: 'b',
+},
+];
+
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+
+var info = null;
+var mobile = window.matchMedia("(max-width: 768px)");
+
+const QUESTION1 = "question1";
+const QUESTION2 = "question2";
+const QUESTION3 = "question3";
+const QUESTION4 = "question4";
+const THEMLUOT = "themluot";
+const MATLUOT = "matluot";
+const SOSGU = "sosgu";
+const BINHNUOCSGU = "binhnuocsgu";
+
+//Object that stores values of minimum and maximum angle for a value
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+const createArrayRandomValue = () => {
+    const arr = [];
+    for(var i=0;i<20; i++)
+        arr.push(QUESTION1)
+    for(var i=0;i<20; i++)
+        arr.push(QUESTION2)
+    for(var i=0;i<20; i++)
+        arr.push(QUESTION3)
+    for(var i=0;i<20; i++)
+        arr.push(QUESTION4)
+    for(var i=0;i<3; i++)
+        arr.push(BINHNUOCSGU)
+    for(var i=0;i<5; i++)
+        arr.push(SOSGU)
+    for(var i=0;i<6; i++)
+        arr.push(MATLUOT)
+    for(var i=0;i<6; i++)
+        arr.push(THEMLUOT)
+    return shuffle(arr);
+}
+
+function randomInRange(start,end){
+    return Math.floor(Math.random() * (end - start + 1) + start);
+}
+
+function randomWithProbability() {
+    var notRandomValues = createArrayRandomValue();
+    var idx = Math.floor(Math.random() * notRandomValues.length);
+    var randomDegree = 0;
+    console.log(notRandomValues[idx]);
+    switch (notRandomValues[idx]) {
+        case BINHNUOCSGU:
+            randomDegree = randomInRange(0, 45);
+            break;
+        case QUESTION1:
+            randomDegree = randomInRange(46, 90);
+            break;
+        case THEMLUOT:
+            randomDegree = randomInRange(91, 135);
+            break;
+        case QUESTION4:
+            randomDegree = randomInRange(136, 180);
+            break;
+        case MATLUOT:
+            randomDegree = randomInRange(181, 225);
+            break;
+        case QUESTION3:
+            randomDegree = randomInRange(226, 270);
+            break;
+        case SOSGU:
+            randomDegree = randomInRange(271, 315);
+            break;
+        case QUESTION2:
+            randomDegree = randomInRange(316, 360);  
+            break;      
+    }
+
+    return randomDegree;
+}
+
+const rotationValues = [
+  { minDegree: 0, maxDegree: 45, value: BINHNUOCSGU },
+  { minDegree: 46, maxDegree: 90, value: QUESTION1 },
+  { minDegree: 91, maxDegree: 135, value: THEMLUOT },
+  { minDegree: 136, maxDegree: 180, value: QUESTION4 },
+  { minDegree: 181, maxDegree: 225, value: MATLUOT },
+  { minDegree: 226, maxDegree: 270, value: QUESTION3 },
+  { minDegree: 271, maxDegree: 315, value: SOSGU },
+  { minDegree: 316, maxDegree: 360, value: QUESTION2 },
+];
+//Size of each piece
+const data = [16, 16, 16, 16, 16, 16, 16, 16];
+//background color for each piece
+var pieColors = [
+  "#176ebb",
+  "#73c0e8",
+  "#176ebb",
+  "#73c0e8",
+  "#176ebb",
+  "#73c0e8",
+  "#176ebb",
+  "#73c0e8",
+];
+//Create chart
+let myChart = new Chart(wheel, {
+  //Plugin for displaying text on pie chart
+  plugins: [ChartDataLabels],
+  //Chart Type Pie
+  type: "pie",
+  data: {
+    //Labels(values which are to be displayed on chart)
+    labels: [["Câu", "hỏi"], ["Bình", "nước"], ["Câu", "hỏi"], ["Sổ", "tay"], ["Câu", "hỏi"], ["Mất", "lượt"],["Câu", "hỏi"], ["Thêm", "lượt"]],
+    //Settings for dataset/pie
+    datasets: [
+      {
+        backgroundColor: pieColors,
+        data: data,
+      },
+    ],
+  },
+  options: {
+    //Responsive chart
+    responsive: true,
+    animation: { duration: 0 },
+    layout: {
+      padding: 15,
+    },
+    plugins: {
+      //hide tooltip and legend
+      tooltip: false,
+      legend: {
+        display: false,
+      },
+      // labels: {
+      //   textMargin: 12,
+      // },
+      //display labels inside pie chart
+      datalabels: {
+        color: "#ffffff",
+        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+        font: { size: mobile.matches ? 15 : 24  },
+      },
+    },
+    
+  },
+});
+
+const renderQuestion = () => {
+  var index = randomInRange(0,dataQuestion.length-1);
+  while(info.asked.includes(index)) {
+    index = randomInRange(0,dataQuestion.length-1);
+  }
+  info.asked.push(index);
+  console.log('index', index);
+  updateData();
+  const randomQuestion = dataQuestion[index];
+
+  const result = `
+  <div class="container">
+    <h2>${randomQuestion.question}</h2>
+    <form name="frmQuestion"> 
+      <div class="row">
+        <div class="col-12 col-6-md">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="radBtn" id="radBtnA">
+            <label class="form-check-label" for="radBtnA">
+              A.${randomQuestion.a}
+            </label>
+          </div>
+        </div>
+        <div class="col-12 col-6-md">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="radBtn" id="radBtnB">
+            <label class="form-check-label" for="radBtnB">
+              B.${randomQuestion.b}
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-6-md">
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="radBtn" id="radBtnC">
+            <label class="form-check-label" for="radBtnC">
+              C.${randomQuestion.c}
+            </label>
+          </div>
+        </div>
+        <div class="col-12 col-6-md">
+          ${randomQuestion.d ? `
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="radBtn" id="radBtnD">
+            <label class="form-check-label" for="radBtnD">
+              D.${randomQuestion.d ? randomQuestion.d : ''}
+            </label>
+          </div>
+          ` : ''}
+        </div>
+      </div>
+      <button type="button" class="btn btn-success mt-3" onClick="submitAnswer('${randomQuestion.answer}')">Xác nhận</button>
+    </form>
+  </div>`;
+  return result;
+}
+
+const submitAnswer = answer => {
+  let result = false;;
+  if(
+    (answer === 'a' && document.getElementById('radBtnA').checked) ||
+    (answer === 'b' && document.getElementById('radBtnB').checked) ||
+    (answer === 'c' && document.getElementById('radBtnC').checked) ||
+    (answer === 'd' && document.getElementById('radBtnD').checked)
+  )
+    result = true;
+
+  if(result){    
+    finalValue.innerHTML = `<p>Chúc mừng bạn đã trả lời đúng</p>`;
+    info.soCauDung ++;
+    updateData();    
+  }
+  else 
+    finalValue.innerHTML = `<p>Bạn đã trả lời sai</p>`;    
+
+  if(info.soLuotChoi > 0)
+    wheelContainer.style.display = "block";
+}
+
+const renderValue = value => {
+  let result;
+
+  switch(value) {
+    case BINHNUOCSGU:
+      result = "<p>Chúc mừng bạn đã trúng 1 bình nước SGU</p>";
+      break;
+    case SOSGU:
+      result = "<p>Chúc mừng bạn đã trúng 1 sổ tay SGU</p>";
+      break;
+    case MATLUOT:
+      result = "<p>Xin chia buồn, bạn đã bị mất 1 lượt chơi</p>";
+      break;
+    case THEMLUOT:
+      result = "<p>Chúc mừng bạn đã được thêm 1 lượt chơi</p>";
+      info.soLuotChoi ++;
+      break;
+    default:
+      wheelContainer.style.display = "none";
+      result= renderQuestion();
+      break;
+  }
+  info.soLuotChoi --;
+  updateData();
+
+  return result;
+}
+
+//display value based on the randomAngle
+const valueGenerator = (angleValue) => {
+  for (let i of rotationValues) {
+    //if the angleValue is between min and max then display it
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      finalValue.innerHTML = renderValue(i.value);
+      spinBtn.disabled = false;
+      break;
+    }
+  }
+};
+
+//Spinner count
+let count = 0;
+//100 rotations for animation and last rotation for result
+let resultValue = 101;
+//Start spinning
+spinBtn.addEventListener("click", () => {
+  spinBtn.disabled = true;
+  //Empty final value
+  finalValue.innerHTML = `<p>Good Luck!</p>`;
+  //Generate random degrees to stop at
+//   let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+    let randomDegree = randomWithProbability();
+
+  //Interval for rotation animation
+  let rotationInterval = window.setInterval(() => {
+    //Set rotation for piechart
+    /*
+    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
+    */
+    myChart.options.rotation = myChart.options.rotation + resultValue;
+    //Update chart with new value;
+    myChart.update();
+    //If rotation>360 reset it back to 0
+    if (myChart.options.rotation >= 360) {
+      count += 1;
+      resultValue -= 5;
+      myChart.options.rotation = 0;
+    } else if (count > 15 && myChart.options.rotation == randomDegree) {
+      valueGenerator(randomDegree);
+      clearInterval(rotationInterval);
+      count = 0;
+      resultValue = 101;
+    }
+  }, 10);
+});
+
+const login = () => {
+  const txtMSSV = document.getElementById('txtMSSV').value;
+  const txtName = document.getElementById('txtName').value;
+
+  if(txtMSSV.length != 10 || txtMSSV[0] != '3' || txtName.length < 5){
+    alert('Mã số sinh viên hoặc họ tên bị sai. Vui lòng nhập lại chính xác!');
+    return;
+  }
+
+  let DSSV = localStorage.getItem('DSSV') ? JSON.parse(localStorage.getItem('DSSV')) : [{ mssv : txtMSSV, name: txtName, soLuotChoi: 3, soCauDung: 0, asked: [] }];
+  
+  if(typeof DSSV != Object)
+    DSSV = [{ mssv : txtMSSV, name: txtName, soLuotChoi: 3, soCauDung: 0, asked: [] }];
+
+  DSSV.forEach(sv => {
+    if(sv.mssv === txtMSSV){
+      info = sv;
+    }
+    
+  });
+  if(info === null){
+    info = { mssv : txtMSSV, name: txtName, soLuotChoi: 3, soCauDung: 0, asked: [] };
+    DSSV = DSSV.push(info);
+  }
+  console.log('DSSV', DSSV, typeof DSSV);
+  console.log('info', info);
+
+  const formLogin = document.getElementById('frmLogin');
+  formLogin.style.display = 'none';
+
+  const pWelcome = document.getElementById('pWelcome');
+  const pLuotChoi = document.getElementById('pLuotChoi');
+  const pSoCauDung = document.getElementById('pSoCauDung');
+  const wheelContainer = document.getElementById('wheelContainer');
+
+  pWelcome.innerHTML = `Xin chào bạn ${info.name}. <a href="/" >Đăng xuất</a>`;
+  pLuotChoi.innerHTML = `Bạn còn ${info.soLuotChoi} lượt chơi.`;
+  pSoCauDung.innerHTML = `Bạn đã trả lời đúng ${info.soCauDung} câu.`;
+  info.soLuotChoi > 0 ? wheelContainer.style.display = "block" : null;
+
+  localStorage.setItem('DSSV', JSON.stringify(DSSV));
+  console.log(txtMSSV, info);
+}
+
+const updateData = () => {
+  let newDSSV = null;
+  if(localStorage.getItem('DSSV'))
+   newDSSV = JSON.parse(localStorage.getItem('DSSV')) 
+  else
+    return;
+  for(var i=0; i<newDSSV.length; i++) {
+    if(newDSSV[i].mssv === info.mssv)
+    newDSSV[i] = info;
+  }
+  pLuotChoi.innerHTML = `Bạn còn ${info.soLuotChoi} lượt chơi.`;
+  pSoCauDung.innerHTML = `Bạn đã trả lời đúng ${info.soCauDung} câu.`;
+
+  if(info.soLuotChoi === 0)
+    wheelContainer.style.display = "none";
+
+  localStorage.setItem('DSSV', JSON.stringify(newDSSV));
+}
